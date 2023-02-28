99e60f67445f2fb7206abbc0540b9f623d8937d6
WhiteMorro1177 <jackforest228@gmail.com>
2023-02-28 15:47:27 +0300

    successfull test command extraction

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ microphone_index = 1
 
 options = {
     "alias": {"григорий", "гриша", "григ"},  # assistant names
-    "tbr": {""},  # to be remove
+    "tbr": {"скажи", "покажи", ""},  # to be remove
     "cmd": {  # assistant recognizable commands
         "time": {"сколько время", "который час"},
         "open": {"открой", "запусти"},
--- fw_text.py
+++ fw_text.py
@@ -1,6 +1,5 @@
-from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
-
+import config
 import os
 
 files = os.listdir("D:\\Programs\\py\\voice_assistant\\")
@@ -13,5 +12,21 @@ first arg - string with recognized voice
 second arg - collection with choices
 
 '''
-print(a)
+# print(a)
+
+cmd = "григорий скажи сколько сейчас время"  # распознанная фраза
+
+cfg = config.options
+phrase_extraction = dict()
+
+for option in config.options:
+    phrase_extraction[option] = process.extractOne(cmd, cfg[option])
+
+
+final_cmd = process.extractOne(cmd, phrase_extraction["cmd"][0])
+if (final_cmd[1] > 50):
+    if (final_cmd[0] in cfg["cmd"]["time"]):
+        print("Time: ")
+        #  TODO(Show system time)
+
 
