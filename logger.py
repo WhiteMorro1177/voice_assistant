d932a2dc0053380cf0638a9ed669f64cbf095ad8
WhiteMorro1177 <jackforest228@gmail.com>
2023-05-30 19:26:56 +0300

    clean up - v1.0

--- logger.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-class Logger():
-    path = "E:\\Programming\\Ever\\py\\voice_assistant\\data\\log.txt"
-
-    def log(self, text):
-        with open(self.path) as log_file:
-            log_file.write(f"Time: {datetime.datetime.now()}, Text: {text}\n")
